54f7c09ef31c3ba995556c32eeb4d0a28537a318
Filipe Augusto Gonçalves <filipe@edu.itajai.sc.gov.br>
2020-05-01 17:06:41 -0300

    vuetify lint config

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -7,8 +7,12 @@ module.exports = {
   parserOptions: {
     parser: "babel-eslint"
   },
+  plugins: ["vuetify"],
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off"
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "vuetify/no-deprecated-classes": "error",
+    "vuetify/grid-unknown-attributes": "error",
+    "vuetify/no-legacy-grid": "error"
   }
 };
