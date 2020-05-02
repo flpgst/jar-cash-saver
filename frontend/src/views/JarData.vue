0f91075b1e565ca441851da044716b10e9275137
Jhony Almeida <jhony.almeida@navita.com.br>
2020-05-02 18:20:40 -0300

    Populando form com dados do jarro, id passado via propriedade

--- frontend/src/views/JarData.vue
+++ frontend/src/views/JarData.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-container>
+  <v-container v-if="jar">
     <v-row>
       <v-col cols="12" class="font-weight-black text-center pb-0">
         {{ jar.name }}
@@ -7,16 +7,16 @@
     </v-row>
     <v-row>
       <v-col cols="12" class="text-center">
-        <v-badge :color="jar.locker.color" :icon="jar.locker.icon" overlap>
-          <v-tooltip :color="jar.locker.color" right>
+        <v-badge color="#f7b500" :icon="getLockIcon(jar)" overlap>
+          <v-tooltip color="#f7b500" right>
             <template v-slot:activator="{ on }">
               <v-icon v-on="on" size="80" :color="jar.color">{{
-                jar.icon
+                getIcon(jar)
               }}</v-icon>
             </template>
-            <span class="caption"
-              >O cadeado abrirá apenas quando o jarro estiver 70% completo</span
-            >
+            <span class="caption">
+              O cadeado abrirá apenas quando o jarro estiver 70% completo
+            </span>
           </v-tooltip>
         </v-badge>
       </v-col>
@@ -24,10 +24,11 @@
     <v-row class="ma-0 d-flex justify-center">
       <v-col class="justify-self-center pa-0" cols="10">
         <v-text-field
-          value="2.000"
-          label="Meta Estipulada"
+          :value="jar.targetValue"
+          label="Meta estipulada"
           :readonly="editTargetValue"
           dense
+          prefix="R$"
           :append-outer-icon="editableIcon(editTargetValue)"
           @click:append-outer="editTargetValue = !editTargetValue"
         ></v-text-field>
@@ -36,9 +37,10 @@
     <v-row class="ma-0 d-flex justify-center">
       <v-col class="justify-self-center pa-0" cols="10">
         <v-text-field
-          value="166"
-          label="Valor poupado mensal"
+          :value="account.monthlySaving * jar.fraction"
+          label="Valor depositado por mês"
           readonly
+          prefix="R$"
           dense
         ></v-text-field>
       </v-col>
@@ -46,27 +48,18 @@
     <v-row class="ma-0 d-flex justify-center">
       <v-col class="justify-self-center pa-0" cols="10">
         <v-text-field
-          value="500"
+          :value="jar.currentValue"
           label="Valor poupado até o momento"
           readonly
           dense
+          prefix="R$"
         ></v-text-field>
       </v-col>
     </v-row>
     <v-row class="ma-0 d-flex justify-center">
       <v-col class="justify-self-center pa-0" cols="10">
         <v-text-field
-          value="6%"
-          label="Total Rendido"
-          readonly
-          dense
-        ></v-text-field>
-      </v-col>
-    </v-row>
-    <v-row class="ma-0 d-flex justify-center">
-      <v-col class="justify-self-center pa-0" cols="10">
-        <v-text-field
-          value="20/12/2020"
+          :value="jar.dueDate"
           label="Data para atingir a meta"
           :readonly="editDueDate"
           dense
@@ -79,6 +72,8 @@
 </template>
 
 <script>
+import api from "../api";
+
 export default {
   props: {
     id: String
@@ -86,24 +81,33 @@ export default {
   data: () => ({
     editTargetValue: true,
     editDueDate: true,
-    jar: {
-      id: 1,
-      name: "Jarro da Reserva",
-      icon: "mdi-cup",
-      path: "jar/1",
-      locker: {
-        icon: "mdi-lock",
-        color: "#f7b500"
-      }
-    }
+    account: null,
+    jar: null
   }),
   methods: {
+    getIcon(jar) {
+      if (jar.currentValue === 0) {
+        return "mdi-cup-outline";
+      } else {
+        return "mdi-cup";
+      }
+    },
+    getLockIcon(jar) {
+      return jar.targetValue * 0.7 < jar.currentValue
+        ? "mdi-lock"
+        : "mdi-exclamation-thick";
+    },
     editing() {
       return true;
     },
     editableIcon(editable) {
       return editable ? "mdi-pencil" : "mdi-content-save";
     }
+  },
+  mounted() {
+    const id = parseInt(this.id);
+    this.account = api.getAccount();
+    this.jar = api.getJarById(id);
   }
 };
 </script>
