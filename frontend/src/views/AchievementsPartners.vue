58371920846ea9f9e53b9f54c20915c3b7495213
Filipe Augusto Gonçalves <filipe@edu.itajai.sc.gov.br>
2020-05-02 00:07:55 -0300

    achievements screen

---
+++ frontend/src/views/AchievementsPartners.vue
@@ -0,0 +1,51 @@
+<template>
+  <v-container>
+    <v-row class="d-flex justify-center">
+      <v-col cols="12" class="d-flex justify-center">
+        Nós também não resistimos a uma comprinha! Você pode usar as moedas que
+        conquistou para comprar produtos e serviços com nossos parceiros.
+        Confira as oportunidades.
+      </v-col>
+    </v-row>
+
+    <!-- Partners -->
+    <v-row>
+      <v-col v-for="(item, i) in items" :key="i" cols="6">
+        <v-card flat :href="item.url" target="_blank">
+          <v-system-bar :color="item.color" height="10" />
+          <v-container class="py-2">
+            <v-img
+              :src="require(`../assets/partners/achievements/${item.src}`)"
+            ></v-img>
+          </v-container>
+        </v-card>
+      </v-col>
+      <!--  -->
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    items: [
+      {
+        src: "burguer-king.jpg",
+        url: "http://cupons.burgerking.com.br",
+        color: "#6dd400"
+      },
+      {
+        src: "udacity.jpg",
+        url: "https://www.udacity.com/scholarships",
+        color: "#e02020"
+      },
+
+      {
+        src: "league-of-legends.jpg",
+        url: "http://br.leagueoflegends.com/pt-br/news/merch/",
+        color: "#f7b500"
+      }
+    ]
+  })
+};
+</script>
