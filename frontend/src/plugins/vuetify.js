8baabc48a0ac5d7f81b97cd5df03a3db4e2445af
Filipe Augusto Gonçalves <filipe@edu.itajai.sc.gov.br>
2020-05-02 21:21:52 -0300

    change locale

--- frontend/src/plugins/vuetify.js
+++ frontend/src/plugins/vuetify.js
@@ -1,16 +1,29 @@
 import Vue from "vue";
 import Vuetify from "vuetify/lib";
+import pt from "vuetify/es5/locale/pt";
 
 Vue.use(Vuetify);
 
+Vue.component("my-component", {
+  methods: {
+    changeLocale() {
+      this.$vuetify.lang.current = "pt";
+    }
+  }
+});
+
 export default new Vuetify({
+  lang: {
+    locales: { pt },
+    current: "pt"
+  },
   theme: {
     options: {
       customProperties: true
     },
     themes: {
       light: {
-        background: "#aeefaa"
+        background: "#B9FBAF"
       }
     }
   }
